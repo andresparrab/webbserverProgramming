b2322d75bebb04f0e18114762a8ed32da2eee8db
andresparrab <andres.parrab@gmail.com>
2021-05-31 06:46:53 +0200

    zxcvzc

--- public_html/myBlog/wordpress/wp-content/themes/MyBlogTheme/sidebar.php
+++ public_html/myBlog/wordpress/wp-content/themes/MyBlogTheme/sidebar.php
@@ -1,5 +1,6 @@
 <aside class="sidebar">
     <p>
-        sidebar
+    In truth even adepts meet challenges in life; it is their mindful equanimity in the face of difficulties that ease their path.
+Lao Tzu, ‎A J Girling, Tao Te Ching
     </p>
 </aside>
